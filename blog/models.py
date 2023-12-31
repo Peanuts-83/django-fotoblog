26d213ed1e9cbdc3c5c56e4fa796209ead1a7bef
tom <tranque@free.fr>
2023-12-31 10:09:47 +0100

    remove file form disk on delete

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.conf import settings
+import os
 
 class Photo(models.Model):
     image = models.ImageField(default="", max_length=256)
     caption = models.CharField(max_length=128, blank=True)
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    #  Delete file from hard-drive before db delete
+    def delete(self, *args, **kwargs):
+        if os.path.isfile(settings.MEDIA_ROOT + self.image.name):
+            os.remove(settings.MEDIA_ROOT + self.image.name)
+        super(Photo, self).delete(*args, **kwargs)
 
 class Blog(models.Model):
     photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
